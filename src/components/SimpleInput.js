fa6daea0158b7f7a449665893089047c0c5b5629
React_Teaching_Project <priyassss789@gmail.com>
2021-10-22 16:08:23 +0530

    React to Lost Focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,36 +1,45 @@
 import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  //Using useRef hook 
+  //Using useRef hook
   const nameInputRef = useRef();
-  //Using usestate hook 
-  const [enteredName, setEnteredName] = useState('');
+  //Using usestate hook
+  const [enteredName, setEnteredName] = useState("");
   //initially entered name is not valid so setting to false
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   useEffect(() => {
     if (enteredNameIsValid) {
-      console.log('Input name is valid');
+      console.log("Input name is valid");
     }
-  }, [enteredNameIsValid])
+  }, [enteredNameIsValid]);
 
-
-  const nameInputChangeHandler = event => {
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formSubmissionHandler = event => {
-    event.preventDefault();
-
-    setEnteredNameTouched(true)
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
-    };
+    }
 
-    setEnteredNameIsValid(true)
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
     //use ref
@@ -39,26 +48,33 @@ const SimpleInput = (props) => {
     console.log(enteredValue);
 
     //reseting the entered value using UseStae
-    setEnteredName('');
+    setEnteredName("");
 
     //reseting the entredvalue using useRef
-    //nameInputRef.current.value='';//Bad way  dont manipulate the dom 
-
+    //nameInputRef.current.value='';//Bad way  dont manipulate the dom
   };
 
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
-  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : 'form-control ';
-
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control ";
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
-        {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
@@ -67,4 +83,4 @@ const SimpleInput = (props) => {
   );
 };
 
-export default SimpleInput; 
+export default SimpleInput;
